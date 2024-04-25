1161c6ecca41d3e3e504486c8151cbdb9bcb3281
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-04-24 21:28:21 -0400

    improve

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,17 +1,26 @@
 'use client'
 
-import * as React from 'react'
-import { ThemeProvider as NextThemesProvider } from 'next-themes'
-import { ThemeProviderProps } from 'next-themes/dist/types'
+import React, { forwardRef, PropsWith, ReactNode, useMemo } from 'react'
+import { ThemeProvider as NextThemesProvider, ThemeProviderProps } from 'next-themes'
 import { SidebarProvider } from '@/lib/hooks/use-sidebar'
 import { TooltipProvider } from '@/components/ui/tooltip'
 
-export function Providers({ children, ...props }: ThemeProviderProps) {
+type ProvidersProps = PropsWith<ThemeProviderProps, { children: ReactNode }>
+
+const Providers = forwardRef<HTMLDivElement, ProvidersProps>(({ children, ...props }, ref) => {
+  const sidebarProvider = useMemo(() => <SidebarProvider key="sidebar" />, [children])
+  const tooltipProvider = useMemo(() => <TooltipProvider key="tooltip" />, [children])
+
   return (
-    <NextThemesProvider {...props}>
-      <SidebarProvider>
-        <TooltipProvider>{children}</TooltipProvider>
-      </SidebarProvider>
+    <NextThemesProvider {...props} ref={ref}>
+      {sidebarProvider}
+      {tooltipProvider}
+      {children}
     </NextThemesProvider>
   )
-}
+})
+
+Providers.displayName = 'Providers'
+
+export default Providers
+
