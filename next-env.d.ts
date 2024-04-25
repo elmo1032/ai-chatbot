7d219954aacdc58e1afc9f179cc1d597e3ee33cb
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-04-24 21:28:17 -0400

    improve

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,5 +1,10 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/basic-features/typescript for more information.
+declare module 'next' {
+  // Add any custom types or interfaces here
+}
+
+declare module 'next/image-types/global' {
+  // Add any custom types or interfaces here
+}
