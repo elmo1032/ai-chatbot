cb2e9f2a67068653a88863ca3c2df9ff7d9c3a4f
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-04-24 21:28:18 -0400

    improve

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,24 @@
-export { auth as middleware } from './auth'
+// middleware.js
+export const auth = (req, res, next) => {
+  // Add your authentication logic here
+  // For example:
+  // if (req.isAuthenticated()) {
+  //   return next();
+  // }
+  // return res.status(401).send('Unauthorized');
+};
+
+// next-middlewares.js
+import nextConnect from 'next-connect';
+import { auth } from './middleware';
+
+const middleware = nextConnect();
+
+middleware.use(auth);
 
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
-}
+};
+
+export default middleware;
+
