91e8ffca9db1d8f8c49335102b3185f33a4eb98a
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-04-25 14:18:24 -0400

    comment

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,11 @@
+// This module exports an object with a single property called "plugins".
+// The "plugins" property is an object that contains two plugins:
+// 1. "tailwindcss": This plugin is used for utilities-first CSS framework. It allows you to quickly and easily build custom designs with a low-level, flexible set of utility classes.
+// 2. "autoprefixer": This plugin is used to parse CSS and add vendor prefixes to CSS rules using values from Can I Use. It is recommended to use this plugin in conjunction with tailwindcss to ensure maximum browser compatibility.
+
 module.exports = {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    tailwindcss: {}, // Object for tailwindcss plugin configuration
+    autoprefixer: {}, // Object for autoprefixer plugin configuration
   },
 }
