99b384b68594b58017a2c3066a627ef2573d0d93
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-04-25 14:18:21 -0400

    comment

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,8 @@
+// Importing the named exports 'GET' and 'POST' from '@/auth' module.
+// These could be functions or objects related to authentication for HTTP requests.
 export { GET, POST } from '@/auth'
+
+// Setting the runtime environment to 'edge'. This indicates that the function will be executed in an edge runtime environment,
+// which is designed to provide low-latency and high-performance for serverless functions.
 export const runtime = 'edge'
+
