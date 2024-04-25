a83796a1944e1e3335a3c320ac174ffbb685a18d
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-04-24 21:28:19 -0400

    improve

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,19 +1,33 @@
 'use client'
 
 import * as React from 'react'
-
 import { useSidebar } from '@/lib/hooks/use-sidebar'
 import { cn } from '@/lib/utils'
+import { SidebarState } from '@/types'
 
-export interface SidebarProps extends React.ComponentProps<'div'> {}
+export interface SidebarProps
+  extends React.ComponentProps<'div'>,
+    Pick<Sidebar, 'sideBarState' | 'sideBarTransition'> {
+  sideBarOpen?: boolean
+  sideBarLoading?: boolean
+}
 
-export function Sidebar({ className, children }: SidebarProps) {
-  const { isSidebarOpen, isLoading } = useSidebar()
-
+export function Sidebar({
+  className,
+  sideBarState,
+  sideBarTransition,
+  sideBarOpen = useSidebar((state) => state.isSidebarOpen),
+  sideBarLoading = useSidebar((state) => state.isLoading),
+  children,
+}: SidebarProps) {
   return (
     <div
-      data-state={isSidebarOpen && !isLoading ? 'open' : 'closed'}
-      className={cn(className, 'h-full flex-col dark:bg-zinc-950')}
+      data-state={sideBarState || (sideBarOpen && !sideBarLoading ? 'open' : 'closed')}
+      className={cn(
+        className,
+        'h-full flex-col transition-transform duration-300',
+        sideBarTransition || 'ease-in-out'
+      )}
     >
       {children}
     </div>
